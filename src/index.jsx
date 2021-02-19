1a357aa78556e3d0b12737068da9a68298fbe109
vaechy <76763229@qq.com>
2021-02-19 08:55:36 +0800

    0.0.1

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-function Mycomponent() {
-    console.log(this);
-    return <h2>hello react</h2>
-}
-ReactDOM.render(
-    <Mycomponent/>
-    , document.getElementById("app"))
+
